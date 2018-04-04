39c8a2ee9670a8d65b25bf7afba8fdbbe6992dc3
Arbuzee <arbuzemail@gmail.com>
2018-04-04 16:27:59 +0200

    UI improvements

---
+++ Tic Tac Toe/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    public void OpenMenu()
+    {
+        Application.LoadLevel("Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
